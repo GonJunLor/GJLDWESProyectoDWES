2a27b10df5dcbc42cada568c16159544d7673af8
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-20 22:08:48 +0100

    style: :art: modificar tarjetas

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -14,7 +14,7 @@
     <meta name="generator" content="visual studio code">
     <link rel="icon" type="image/png" href="webroot/media/favicon/favicon-32x32.png">
     <link rel="stylesheet" href="webroot/css/fonts.css">
-    <link rel="stylesheet" href="webroot/css/estilosHome.css">
+    <link rel="stylesheet" href="webroot/css/estilos.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" type="text/css" rel="stylesheet">
     <link rel="alternate" href="rss.xml" type="application/rss+xml" title="RSS">
     <title>Gonzalo Junquera Lorenzo</title>
@@ -28,52 +28,80 @@
     </nav>
     <main>
         <section>
-            <header><h2>Documentos</h2></header>
             <div>
                 <article class="practica">
                     <iframe src="doc/EstudioTema1.pdf"></iframe>
                     <a href="doc/EstudioTema1.pdf" target="_blank">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 1</p>
+                            <p>DESARROLLO WEB EN ENTORNO SERVIDOR</p>
+                        </div>
                         <div class="superpuesto"></div>
-                        <h4>UT1: DESARROLLO WEB EN ENTORNO SERVIDOR</h4>
+                        <h4>Estudio Tema 1</h4>
                     </a>
                 </article>
+                <!-- <article class="practica">
+                    <iframe src="doc/EstudioTema1.pdf"></iframe>
+                    <a href="" target="_blank">
+                        <div class="tituloPractica">
+                            <p>Tema 1</p>
+                            <p>DESARROLLO WEB EN ENTORNO SERVIDOR</p>
+                        </div>
+                        <div class="superpuesto"></div>
+                        <h4>Estudio Framework</h4>
+                    </a>
+                </article> -->
                 <article class="practica">
                     <iframe src="doc/README.html"></iframe>
                     <a href="https://github.com/GonJunLor/GJLDAWProyectoDAW/blob/master/README.md" target="_blank">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 2</p>
+                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</p>
+                        </div>
                         <div class="superpuesto"></div>
-                        <h4>UT2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</h4>
+                        <h4>Documentación ED y EE</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema3/indexProyectoTema3.php"></iframe>
                     <a href="../GJLDWESProyectoTema3/indexProyectoTema3.php">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 3</p>
+                            <p>CARACTERÍSTICAS DEL LENGUAJE PHP</p>
+                        </div>
                         <div class="superpuesto"></div>
-                        <h4>UT3: CARACTERÍSTICAS DEL LENGUAJE PHP</h4>
+                        <h4>Ejercicios</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema4/indexProyectoTema4.php"></iframe>
                     <a href="../GJLDWESProyectoTema4/indexProyectoTema4.php">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 4</p>
+                            <p>TÉCNICAS DE ACCESO A DATOS EN PHP</p>
+                        </div>
                         <div class="superpuesto"></div>
-                        <h4>UT4: TÉCNICAS DE ACCESO A DATOS EN PHP </h4>
+                        <h4>Ejercicios</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema5/indexProyectoTema5.php"></iframe>
                     <a href="../GJLDWESProyectoTema5/indexProyectoTema5.php">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 5</p>
+                            <p>DESARROLLO DE APLICACIONES WEB</p>
+                        </div>
                         <div class="superpuesto"></div>
-                        <h4>UT5: DESARROLLO DE APLICACIONES WEB</h4>
+                        <h4>Ejercicios</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESLoginLogoffTema5/indexLoginLogoffTema5.php"></iframe>
                     <a href="../GJLDWESLoginLogoffTema5/indexLoginLogoffTema5.php">
-                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="tituloPractica">
+                            <p>Tema 5</p>
+                            <p>DESARROLLO DE APLICACIONES WEB</p>
+                        </div>
                         <div class="superpuesto"></div>
                         <h4>Login Logoff Tema 5</h4>
                     </a>
