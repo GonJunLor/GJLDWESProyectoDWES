36e092f385027eaa860faffbd547205c77825a45
GonJunLor <gonzalo.junlor@educa.jcyl.es>
2026-01-22 13:51:23 +0100

    style: rediseño tarjetas

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -33,121 +33,121 @@
                     <iframe src="doc/EstudioTema1.pdf"></iframe>
                     <a href="doc/EstudioTema1.pdf" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 1</p>
+                            <p>Estudio Tema 1</p>
                             <p>DESARROLLO WEB EN ENTORNO SERVIDOR</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Estudio Tema 1</h4>
+                        <h4>Tema 1</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="doc/out/Spring Boot.html"></iframe>
                     <a href="https://github.com/GonJunLor/GJLDWESProyectoDWES/blob/developerGJL/doc/Spring_Boot.md" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 1</p>
+                            <p>Framework - Spring Boot</p>
                             <p>DESARROLLO WEB EN ENTORNO SERVIDOR</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Framework - Spring Boot</h4>
+                        <h4>Tema 1</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="doc/README.html"></iframe>
                     <a href="https://github.com/GonJunLor/GJLDAWProyectoDAW/blob/master/README.md" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 2</p>
-                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</p>
+                            <p>Documentación ED y EE</p>
+                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ED y EE</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Documentación ED y EE</h4>
+                        <h4>Tema 2</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="doc/out/Clases y funciones.html"></iframe>
                     <a href="https://github.com/GonJunLor/GJLDWESProyectoDWES/blob/developerGJL/doc/Clases%20y%20funciones.md" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 2</p>
-                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</p>
+                            <p>Clases y funciones PHP</p>
+                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ED y EE</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Clases y funciones PHP</h4>
+                        <h4>Tema 2</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="doc/out/Spring Boot.html"></iframe>
                     <a href="https://github.com/GonJunLor/GJLDWESProyectoDWES/blob/developerGJL/doc/Spring_Boot_ED.md" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 2</p>
-                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</p>
+                            <p>ED - Spring Boot</p>
+                            <p>INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ED y EE</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Entorno de Desarrollo - Spring Boot</h4>
+                        <h4>Tema 2</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema3/indexProyectoTema3.php"></iframe>
                     <a href="../GJLDWESProyectoTema3/indexProyectoTema3.php">
                         <div class="tituloPractica">
-                            <p>Tema 3</p>
+                            <p>Ejercicios T3</p>
                             <p>CARACTERÍSTICAS DEL LENGUAJE PHP</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Ejercicios</h4>
+                        <h4>Tema 3</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema4/indexProyectoTema4.php"></iframe>
                     <a href="../GJLDWESProyectoTema4/indexProyectoTema4.php">
                         <div class="tituloPractica">
-                            <p>Tema 4</p>
+                            <p>Ejercicios T4</p>
                             <p>TÉCNICAS DE ACCESO A DATOS EN PHP</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Ejercicios</h4>
+                        <h4>Tema 4</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESProyectoTema5/indexProyectoTema5.php"></iframe>
                     <a href="../GJLDWESProyectoTema5/indexProyectoTema5.php">
                         <div class="tituloPractica">
-                            <p>Tema 5</p>
+                            <p>Ejercicios T5</p>
                             <p>DESARROLLO DE APLICACIONES WEB</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Ejercicios</h4>
+                        <h4>Tema 5</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESLoginLogoffTema5/indexLoginLogoffTema5.php"></iframe>
                     <a href="../GJLDWESLoginLogoffTema5/indexLoginLogoffTema5.php" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 5</p>
+                            <p>Login Logoff Tema 5</p>
                             <p>DESARROLLO DE APLICACIONES WEB</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Login Logoff Tema 5</h4>
+                        <h4>Tema 5</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESLoginLogoff/indexLoginLogoff.php"></iframe>
                     <a href="../GJLDWESLoginLogoff/indexLoginLogoff.php" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 6</p>
+                            <p>Login Logoff</p>
                             <p>APLICACIONES WEB MULTICAPA</p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Login Logoff</h4>
+                        <h4>Tema 6</h4>
                     </a>
                 </article>
                 <article class="practica">
                     <iframe src="../GJLDWESAplicacionFinal/index.php"></iframe>
                     <a href="../GJLDWESAplicacionFinal/index.php" target="_blank">
                         <div class="tituloPractica">
-                            <p>Tema 8</p>
+                            <p>Aplicación final</p>
                             <p></p>
                         </div>
                         <div class="superpuesto"></div>
-                        <h4>Login Logoff</h4>
+                        <h4>Tema 8</h4>
                     </a>
                 </article>
             </div>
